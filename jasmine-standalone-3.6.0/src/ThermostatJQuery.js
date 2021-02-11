545b44126d28872553bfe08b0ddc8cf45f46713e
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-11 16:42:20 +0000

    Implement decrease temp jQuery

--- jasmine-standalone-3.6.0/src/ThermostatJQuery.js
+++ jasmine-standalone-3.6.0/src/ThermostatJQuery.js
@@ -4,5 +4,10 @@ $(document).ready( () => {
     $('#increase_temp').click( () => {
         thermostat.increaseTemperature();
         console.log(thermostat.currentTemperature);
-  })
+    })
+
+    $('#decrease_temp').click( () => {
+      thermostat.decreaseTemperature();
+      console.log(thermostat.currentTemperature)
+    })
 })
