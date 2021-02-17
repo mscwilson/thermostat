02190b29fc7f9b91d36e0d46fa0fd0d1f5f7e5da
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-17 08:36:43 +0000

    Allow user to enter a city to get the temperature there

--- jasmine-standalone-3.6.0/src/ThermostatJQuery.js
+++ jasmine-standalone-3.6.0/src/ThermostatJQuery.js
@@ -2,6 +2,8 @@ $(document).ready( () => {
     let thermostat = new Thermostat();
     updateTemperatureDisplay()
     updatePowerSavingDisplay()
+    updateLocalTemperatureDisplay()
+    updateNamedCity()
 
 
     $('#increase_temp').click( () => {
@@ -24,12 +26,30 @@ $(document).ready( () => {
       updateTemperatureDisplay()
     })
 
-    $.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=" + API_KEY, function(data) {
-      console.log(data)
+    $("#form-select-city").submit( (event) => {
+      event.preventDefault()
+      let city = $("#entered-city").val()
+      updateLocalTemperatureDisplay(city)
     })
 
 
+  function updateLocalTemperatureDisplay(city = "London") {
+    apiUrl = "http://api.openweathermap.org/data/2.5/weather?q="
+    apiCity = city
+    apiKey = `&APPID=${API_KEY}`
+    apiUnits = "&units=metric"
+    $.get(`${apiUrl}${apiCity}${apiKey}${apiUnits}`, function(data) {
+      console.log(data["main"]["temp"])
+      let result = data["main"]["temp"]
+      $("#outside-temperature").text(result)
+    })
+    updateNamedCity(city)
+  }
 
+  function updateNamedCity(city = "London") {
+    let cityCapitalised = city.charAt(0).toUpperCase() + city.slice(1)
+    $("#chosen-city").text(cityCapitalised)
+  }
 
   function updateTemperatureDisplay() {
     $("#current-temperature").text(thermostat.currentTemperature)
